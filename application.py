15b596e86b6df3623a98e388c4c08b97cd60b32f
Shubham Chourasia <shubham070496@gmail.com>
2019-09-01 05:46:32 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,13 +1,24 @@
 from flask import Flask, request, render_template
+import csv
 
 app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template('my-form.html')
+    return render_template('my-form-csv.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+@app.route('/save-comment', methods=['POST'])
+def save_comment():
+    # This is to make sure the HTTP method is POST and not any other
+    if request.method == 'POST':
+        name = request.form['name']
+        comment = request.form['comment']
+        fieldnames = ['name', 'comment']
+
+        with open('nameList.csv','w') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+
+            # writerow() will write a row in your csv file
+            writer.writerow({'name': name, 'comment': comment})
+
+        return 'Thanks for your input!'
