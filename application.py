23f09be4e34c80fe3e839b67ebb529d1c2f3845b
Shubham Chourasia <shubham070496@gmail.com>
2019-09-01 05:40:28 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template      
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("hello.html")
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def my_form():
+    return render_template('my-form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
